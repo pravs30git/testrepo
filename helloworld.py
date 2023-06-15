a736dd3fbca8574e613a2ec001920e5cea567b51
Praveen Kumar Sharma <praveen.sharma@in.unisys.com>
2023-06-15 19:39:29 +0530

    Update helloworld.py - new code

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,13 @@
-import http.server
-import socketserver
+import os
 
-PORT = 8080
-Handler = http.server.SimpleHTTPRequestHandler
+from flask import Flask
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    target = os.environ.get('TARGET', 'World')
+    return 'Hello {}!\n'.format(target)
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
